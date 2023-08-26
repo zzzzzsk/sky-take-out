ff8d01ab7e21cb7ee5488a7d22acf25016e7ee3b
zzzzzsk <1606845866@qq.com>
2023-08-26 22:55:10 +0800

    day08

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -10,4 +10,7 @@ public interface UserMapper {
     User getByOpenid(String openid);
 
     void insert(User user);
+
+    @Select("select * from user where id = #{id}")
+    User getById(Long id);
 }
